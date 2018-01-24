ffd4dc07022cf7eea73334bb670c744220f33ccb
acendrou <20256888+acendrou@users.noreply.github.com>
2018-01-24 23:12:40 +0100

    improvement

--- auto-screenshot.py
+++ auto-screenshot.py
@@ -1,13 +1,20 @@
 import time
 import PIL
 from PIL import ImageGrab
-x=1
+
+x = 1
 while True:
     image = PIL.ImageGrab.grab()
-    image.save(str(time.localtime()) + '.png', 'png')
-    x = x+1
+    t = time.localtime()
+    year = t.tm_year
+    month = t.tm_mon
+    monday = t.tm_mday
+    hour = t.tm_hour
+    minute = t.tm_min
+    second = t.tm_sec
+
+    image.save(str(year) + '-' + str(month) + '-' + str(monday) + '-' + str(hour) + '_' + str(minute) + '-' + str(
+        second) + '.png', 'png')
+    x = x + 1
     print(x)
     time.sleep(30)
-
-
-
