32e8019c0ac6e046fc11c218532f1462970363fd
acendrou <20256888+acendrou@users.noreply.github.com>
2018-01-26 22:17:09 +0100

    directory creation

--- auto-screenshot.py
+++ auto-screenshot.py
@@ -1,6 +1,30 @@
 import time
 import PIL
 from PIL import ImageGrab
+import os
+from unipath import Path
+
+os.chdir("J:\Sauvegarde")
+
+l = time.localtime()
+year = l.tm_year
+month = l.tm_mon
+monday = l.tm_mday
+
+jour = str(year)+'-'+str(month)+'-'+str(monday)
+
+if Path.exists(jour):
+    print("Dossier déjà existant")
+else:
+    os.mkdir(jour)
+    print("Dossier créé")
+
+dossier = 'J:/Sauvegarde/' + str(year) + '-' + str(month) + '-' + str(monday)
+os.path.normpath(dossier)
+os.chdir(dossier)
+
+print(os.getcwd())
+
 
 x = 1
 while True:
@@ -16,5 +40,5 @@ while True:
     image.save(str(year) + '-' + str(month) + '-' + str(monday) + '-' + str(hour) + '_' + str(minute) + '-' + str(
         second) + '.png', 'png')
     x = x + 1
-    print(x)
+    print("Les captures d'écran sont lancées")
     time.sleep(30)
