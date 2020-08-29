a7f52f09f3fe5c9df10ec36c8efb06ecd4e85ff9
acendrou <acendrou@gmx.fr>
2020-08-29 02:28:38 +0200

    pause + config + dependances

--- auto-screenshot.py
+++ auto-screenshot.py
@@ -1,8 +1,38 @@
 import time
 import os
 from mss import mss
+from pynput.keyboard import Listener, Key
+import configparser
 
-repertoireGeneral = "./capture-ecran"
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+marche = True;
+
+def sur_appui(touche):
+    print("Touche presse : {0}".format(touche))
+    if touche is Key.pause:
+        print("Appui pause !")
+        global marche
+        if  marche:
+            print(marche)
+            marche = False
+        else:
+            print(marche)
+            marche = True
+
+
+
+def sur_relache(touche):
+    print("Touche relache : {0}".format(touche))
+
+
+
+listener = Listener(on_press=sur_appui, on_release=sur_relache)
+listener.start()
+
+repertoireGeneral = config['BASE']['repertoireGeneral']
+print(repertoireGeneral)
 
 if os.path.exists(repertoireGeneral):
     print("Ok le dossier existe")
@@ -29,10 +59,12 @@ while True:
 
     os.chdir(nomJourComplet)
 
-    sct = mss()
+    if marche:
+        sct = mss()
 
-    datePhoto = time.localtime()
-    nom = str(datePhoto.tm_year) + '-' + str(datePhoto.tm_mon) + '-' + str(datePhoto.tm_mday) + '-' + str(datePhoto.tm_hour) + '_' + str(datePhoto.tm_min) + '-' + str(datePhoto.tm_sec) + '.png'
+        datePhoto = time.localtime()
+        nom = str(datePhoto.tm_year) + '-' + str(datePhoto.tm_mon) + '-' + str(datePhoto.tm_mday) + '-' + str(datePhoto.tm_hour) + '_' + str(datePhoto.tm_min) + '-' + str(datePhoto.tm_sec) + '.png'
 
-    sct.shot(output=nom)
+        sct.shot(mon=-1,output=nom)
+
     time.sleep(30)
