7b296ca4e34dda36eaeea599af15ccadc341d568
acendrou <acendrou@gmx.fr>
2020-12-18 02:00:40 +0100

    v3 base

---
+++ V1/src/auto-screenshot.py
@@ -0,0 +1,43 @@
+import time
+import PIL
+from PIL import ImageGrab
+import os
+from unipath import Path
+
+repertoire = "./capture-ecran"
+
+os.chdir(repertoire)
+
+l = time.localtime()
+year = l.tm_year
+month = l.tm_mon
+monday = l.tm_mday
+
+jour = str(year)+'-'+str(month)+'-'+str(monday)
+
+if Path.exists(jour):
+    print("Dossier déjà existant")
+else:
+    os.mkdir(jour)
+    print("Dossier créé")
+
+dossier = repertoire + '\\' + jour
+os.path.normpath(dossier)
+os.chdir(dossier)
+
+
+while True:
+    image = PIL.ImageGrab.grab()
+    t = time.localtime()
+    year = t.tm_year
+    month = t.tm_mon
+    monday = t.tm_mday
+    hour = t.tm_hour
+    minute = t.tm_min
+    second = t.tm_sec
+
+    image.save(str(year) + '-' + str(month) + '-' + str(monday) + '-' + str(hour) + '_' + str(minute) + '-' + str(
+        second) + '.png', 'png')
+    
+    print("Les captures d'écran sont lancées")
+    time.sleep(30)
